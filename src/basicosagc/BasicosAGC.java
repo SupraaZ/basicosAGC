09727a7985a9c3ff3df2c15e224dc2a83132da28
SupraaZ <adriangran17@gmail.com>
2019-11-25 10:02:41 +0100

    numericos hecho

--- src/basicosagc/BasicosAGC.java
+++ src/basicosagc/BasicosAGC.java
@@ -9,13 +9,34 @@ package basicosagc;
  *
  * @author Adrian
  */
-public class BasicosAGC {
 
+public class BasicosAGC {
+public static void numericos() {
+
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.err.println("Valor entero");
+        System.out.println(entero);
+        System.out.println("Valor doble");
+        System.out.println(doble);
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        numericos();
     }
     
 }
