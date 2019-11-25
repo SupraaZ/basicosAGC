42a12037b1bb68dc3e212d382126021d3c1a2375
SupraaZ <adriangran17@gmail.com>
2019-11-25 10:07:37 +0100

    logicos hecho

--- src/basicosagc/BasicosAGC.java
+++ src/basicosagc/BasicosAGC.java
@@ -10,12 +10,24 @@ package basicosagc;
  * @author Adrian
  */
 public class BasicosAGC {
-
+    public static void logicos(){
+        boolean logico=true;
+        int num=4;
+        if (num==4){
+            System.out.println("Es verdadero");
+            logico=true;
+        }
+        else{
+             System.out.println("Es falso");
+            logico=false;
+        }
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        logicos();
     }
     
 }
