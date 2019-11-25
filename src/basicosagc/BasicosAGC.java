465a00ce75561cc530b0c6b785861ead1c826dde
SupraaZ <adriangran17@gmail.com>
2019-11-25 10:04:42 +0100

    Cadenas hecho

--- src/basicosagc/BasicosAGC.java
+++ src/basicosagc/BasicosAGC.java
@@ -10,12 +10,16 @@ package basicosagc;
  * @author Adrian
  */
 public class BasicosAGC {
-
+    public static void Cadenas(){
+        String cadena="Pepe";
+        System.out.println(cadena);
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Cadenas();
     }
     
 }
